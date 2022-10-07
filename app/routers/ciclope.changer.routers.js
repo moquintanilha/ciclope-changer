1fcbd632c6b5a8df67ee0c51958c30585060969b
Marcos Quintanilha <marcos.quintanilha@mercadolivre.com>
2022-10-07 14:52:44 -0300

    API Doc update.

--- app/routers/ciclope.changer.routers.js
+++ app/routers/ciclope.changer.routers.js
@@ -5,15 +5,4 @@ module.exports = app => {
   router.patch("/", changer.registerUpdate);
   
   app.use('/api/action', router);
-  /*
-    #swagger.tags = ['Action']
-    #swagger.description = 'Endpoint to change an FQDN associated with a VPN.' */
-  /*
-    #swagger.parameters['action'] = {
-               in: 'body',
-               description: 'Send a request to DNS service.',
-               required: true,
-               schema: { $ref: "#/definitions/Action" }
-        }
-   */
 };
