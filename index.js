a6a2ac9c55ec17eed4174c60c116c3a1cf94ea6f
Marcos Quintanilha <marcos.quintanilha@mercadolivre.com>
2022-10-11 10:30:04 -0300

    Fix to API docs.

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const Logger = require("./app/utils/utils.logger.js");
-
-if (process.env.NODE_ENV === 'production') {
-  /* istanbul ignore next */
-  require('newrelic');
-}
-
 const express = require("express");
-const http = require('http')
-const cors = require("cors");
-const app = express();
-var corsOptions = {
-  "Origin": "http://localhost:7072",
-};
+const http = require('http');
+const path = require('path');
 const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger_output.json');
+const swaggerFile = require('./docs/specs/swagger_output.json');
+const app = express();
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(Logger.requestLogger);
@@ -29,14 +19,17 @@ app.get('/health-check', function (req, res) {
     });
 });
 
+app.get('/guide/',function(req,res){
+  res.sendFile(path.join(__dirname+'/docs/guide/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 require('./app/routers/ciclope.changer.routers.js')(app)
 /* istanbul ignore if */
 if (!module.parent) {
-  http.createServer(app).listen(3000)
-  console.log("Listening at http://localhost:%s (HTTP)", 3000)
-  app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
+  http.createServer(app).listen(3001)
+  console.log("Listening at http://localhost:%s (HTTP)", 3001)
+  app.use('/specs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
   const server = app.listen(8080, function () {
     const port = server.address().port;
     console.log('App listening at http://localhost:%s', port);
