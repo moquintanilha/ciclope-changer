8fbbd789af1beaa32e3f9f03e2a3136618215767
Marcos Quintanilha <marcos.quintanilha@mercadolivre.com>
2022-10-28 10:48:08 -0300

    header field required

--- app/src/exceptions/verify_token.py
+++ app/src/exceptions/verify_token.py
@@ -3,7 +3,5 @@ from typing import Union
 
 
 def verify_token(x_auth_token: Union[str, None] = Header(default=None)):
-    if x_auth_token == "" or x_auth_token is None:
-        raise HTTPException(status_code=401, detail="Unauthorized")
     if len(x_auth_token) < 65:
         raise HTTPException(status_code=422, detail="Validation error")
